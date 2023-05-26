a097b57040e78d6ec5292b01113c20205b9cf0f5
shiran-sandaruwan-69x <shiran101997@gmail.com>
2023-05-26 22:32:56 +0530

    implement user save method and save role method and get user method

--- src/main/java/lk/shiran/JsonWebTokenJWT/controller/UserController.java
+++ src/main/java/lk/shiran/JsonWebTokenJWT/controller/UserController.java
@@ -1,13 +1,11 @@
 package lk.shiran.JsonWebTokenJWT.controller;
 
+import lk.shiran.JsonWebTokenJWT.dto.AppRoleDTO;
 import lk.shiran.JsonWebTokenJWT.dto.AppUserDTO;
 import lk.shiran.JsonWebTokenJWT.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,24 @@ public class UserController {
     @GetMapping("/users")
     public ResponseEntity<List<AppUserDTO>> getAllUsers(){
         return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<AppUserDTO> saveUser(@RequestBody AppUserDTO userDTO){
+        AppUserDTO appUserDTO = userService.saveUser(userDTO);
+        return ResponseEntity.ok().body(appUserDTO);
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<AppRoleDTO> saveRole(@RequestBody AppRoleDTO roleDTO){
+        AppRoleDTO appRoleDTO = userService.saveRole(roleDTO);
+        return ResponseEntity.ok().body(appRoleDTO);
+    }
+
+    @GetMapping("/user/{username}")
+    public ResponseEntity<AppUserDTO> getUser(@PathVariable String username){
+        AppUserDTO user = userService.getUser(username);
+        return ResponseEntity.ok().body(user);
     }
 
 
