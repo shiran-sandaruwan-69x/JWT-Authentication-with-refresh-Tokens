bbba1665baa1b4a0c0dd728915037390cd603941
shiran-sandaruwan-69x <shiran101997@gmail.com>
2023-05-27 20:34:35 +0530

    implement authenticationManagerBean

--- src/main/java/lk/shiran/JsonWebTokenJWT/security/SecurityConfig.java
+++ src/main/java/lk/shiran/JsonWebTokenJWT/security/SecurityConfig.java
@@ -1,7 +1,10 @@
 package lk.shiran.JsonWebTokenJWT.security;
 
+import lk.shiran.JsonWebTokenJWT.filters.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(null);
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
     }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
 }
