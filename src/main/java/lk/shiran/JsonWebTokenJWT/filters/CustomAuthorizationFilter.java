b1181beed4cb4542bf81b9743ef43e6dcd6328bd
shiran-sandaruwan-69x <shiran101997@gmail.com>
2023-05-28 02:00:35 +0530

    create another filter for verify token / CustomAuthorizationFilter class

--- src/main/java/lk/shiran/JsonWebTokenJWT/filters/CustomAuthorizationFilter.java
+++ src/main/java/lk/shiran/JsonWebTokenJWT/filters/CustomAuthorizationFilter.java
@@ -4,6 +4,9 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -11,7 +14,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import static java.util.Arrays.stream;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
@@ -21,20 +27,42 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
         if (request.getServletPath().equals("/api/userlogin")){
             filterChain.doFilter(request,response);
         }else {
-           try {
+
                // token ekak thiyanawa nam verify karana eka karanne
+
                String authorizationHeader = request.getHeader(AUTHORIZATION);
+
                if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+
+               try {
+
                    String token = authorizationHeader.substring("Bearer ".length());
                    Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
                    JWTVerifier verifier = JWT.require(algorithm).build();
                    DecodedJWT decodedJWT = verifier.verify(token);
 
+                   String username = decodedJWT.getSubject();
+                   String [] roles = decodedJWT.getClaim("roles").asArray(String.class);
 
+                   Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+                   stream(roles).forEach(role -> {
+                       authorities.add(new SimpleGrantedAuthority(role));
+                   });
+
+                   UsernamePasswordAuthenticationToken authenticationToken =
+                           new UsernamePasswordAuthenticationToken(username,null,authorities);
+
+                   SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                   filterChain.doFilter(request,response);
+
+               }catch (Exception e){
+                   System.out.println(e.getMessage());
                }
-           }catch (Exception e){
-               System.out.println(e.getMessage());
-           }
+
+               }else{
+                   filterChain.doFilter(request,response);
+               }
+
         }
     }
 
