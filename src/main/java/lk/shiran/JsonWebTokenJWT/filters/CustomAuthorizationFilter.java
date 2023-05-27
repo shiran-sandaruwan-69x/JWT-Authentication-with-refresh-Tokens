ea21911d8b1629e46e8f7de55f385fab36f8cfbf
shiran-sandaruwan-69x <shiran101997@gmail.com>
2023-05-28 01:47:23 +0530

    create another filter for verify token / CustomAuthorizationFilter class

---
+++ src/main/java/lk/shiran/JsonWebTokenJWT/filters/CustomAuthorizationFilter.java
@@ -0,0 +1,41 @@
+package lk.shiran.JsonWebTokenJWT.filters;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getServletPath().equals("/api/userlogin")){
+            filterChain.doFilter(request,response);
+        }else {
+           try {
+               // token ekak thiyanawa nam verify karana eka karanne
+               String authorizationHeader = request.getHeader(AUTHORIZATION);
+               if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+                   String token = authorizationHeader.substring("Bearer ".length());
+                   Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                   JWTVerifier verifier = JWT.require(algorithm).build();
+                   DecodedJWT decodedJWT = verifier.verify(token);
+
+
+               }
+           }catch (Exception e){
+               System.out.println(e.getMessage());
+           }
+        }
+    }
+
+}
